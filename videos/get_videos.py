b2faa70a120b78a9d478a2a9b13362a5fed778de
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-02-03 18:52:43 +0000

    fix: updated scripts and read me

--- videos/get_videos.py
+++ videos/get_videos.py
@@ -16,18 +16,18 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists('videos/token.json'):
+        creds = Credentials.from_authorized_user_file('videos/token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'videos/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open('videos/token.json', 'w') as token:
             token.write(creds.to_json())
 
     service = build('drive', 'v3', credentials=creds)
@@ -47,7 +47,7 @@ def main():
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
             request = service.files().get_media(fileId=item['id'])
-            fh = io.FileIO(item['name'], 'wb')
+            fh = io.FileIO('videos/'+item['name'], 'wb')
             downloader = MediaIoBaseDownload(fh, request)
             done = False
             while done is False:
