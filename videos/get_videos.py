1c0373d1fb67217073394d5502626e6ecac4213c
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-02-03 18:39:34 +0000

    fix: script to download the whole video data

---
+++ videos/get_videos.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+import io
+import os
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def main():
+    """Shows basic usage of the Drive v3 API."""
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API to list files in the specified folder
+    folder_id = '1vM2FTfz4-hEqJhdOeGmHM1e15jlYNJP8'
+    query = f"'{folder_id}' in parents"
+    results = service.files().list(q=query,
+                                   spaces='drive',
+                                   fields='nextPageToken, files(id, name)').execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+            request = service.files().get_media(fileId=item['id'])
+            fh = io.FileIO(item['name'], 'wb')
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print(f"Download {item['name']} %d%%." % int(status.progress() * 100))
+
+if __name__ == '__main__':
+    main()
