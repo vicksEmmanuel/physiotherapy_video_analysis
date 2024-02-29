eabaeb3222d0835a858c05c2bdbcb07fda268a10
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-02-29 00:29:05 +0000

    fix: more test

---
+++ testing.py
@@ -0,0 +1,24 @@
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import Trainer, seed_everything
+from data_preparation.action_dataset import ActionDataset
+from data_preparation.config import CFG
+from data_preparation.util import get_loader, get_transformer
+import cv2
+import numpy as np
+import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
+import torch
+from torchvision import transforms
+from data_preparation.PackPathwayTransform import PackPathway
+from torch.utils.data import Dataset
+
+from data_preparation.actions import Action
+from model.slowfast_model import SlowFast
+
+
+train = ActionDataset(
+        transforms=get_transformer('train'),
+        num_frames=CFG.num_frames
+    )
