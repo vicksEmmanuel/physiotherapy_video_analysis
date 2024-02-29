70f41acdbe2bf41577a41a32f983b29930f80886
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-02-29 05:07:47 +0000

    fix: used open cv to get clips

--- test_slowfast.py
+++ test_slowfast.py
@@ -7,24 +7,28 @@ from model.slowfast_model import SlowFast  # Ensure this import matches your pro
 from torchvision import transforms
 from data_preparation.config import CFG  # Ensure this import matches your project structure
 from data_preparation.PackPathwayTransform import PackPathway
-from data_preparation.util import get_transformer,get_new_transformer  # Ensure this import matches your project structure
+from data_preparation.util import get_transformer,get_new_transformer, get_video_clip_and_resize  # Ensure this import matches your project structure
 
 # Assuming device setup as before
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+
+# Prepare video capture
+video_path = 'data_preparation/actions/foot and ankle examination/2024-02-14 15-56-41.mp4'
+video = EncodedVideo.from_path(video_path, decode_audio=False)
+video_data = get_video_clip_and_resize(video_path=video_path, start_sec=0, end_sec= int(video.duration))
+
+print(f"video data  {video_data}")
 
 # Load your trained SlowFast model
 model = SlowFast.load_from_checkpoint("checkpoints/last.ckpt")
 model.eval()
 model.to(device)
 
-# Prepare video capture
-video_path = 'archery.mp4'
-video = EncodedVideo.from_path(video_path)
-
 total_duration = int(video.duration)  # Total duration in seconds
 transform = get_new_transformer('test')
 actions_per_second = []
-
 
 
 for start_sec in range(0, total_duration):
