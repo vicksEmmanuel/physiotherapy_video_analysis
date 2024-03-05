8cc682e3317415fd17b585e01e4f3beff058ea33
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-05 01:41:27 +0000

    more on testing

--- test_slowfast_2.py
+++ test_slowfast_2.py
@@ -6,11 +6,6 @@ from detectron2.config import get_cfg
 from pytorchvideo.models.hub import slow_r50_detection 
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
-from pytorchvideo.transforms.functional import (
-    uniform_temporal_subsample,
-    short_side_scale_with_boxes,
-    clip_boxes_to_image,
-)
 from torchvision.transforms._functional_video import normalize
 import numpy as np
 from data_preparation.util_2 import  get_video_clip_and_resize # Ensure this import matches your project structure
@@ -60,62 +55,6 @@ def get_person_bboxes(inp_img, predictor):
     classes = np.array(predictions.pred_classes.tolist() if predictions.has("pred_classes") else None)
     predicted_boxes = boxes[np.logical_and(classes==0, scores>0.75 )].tensor.cpu() # only person
     return predicted_boxes
-
-def ava_inference_transform(
-    clip,
-    boxes,
-    num_frames = 4, #if using slowfast_r50_detection, change this to 32
-    crop_size = 256,
-    data_mean = [0.45, 0.45, 0.45],
-    data_std = [0.225, 0.225, 0.225],
-    slow_fast_alpha = None, #if using slowfast_r50_detection, change this to 4
-):
-
-    boxes = np.array(boxes)
-    ori_boxes = boxes.copy()
-
-    # Image [0, 255] -> [0, 1].
-    clip = uniform_temporal_subsample(clip, num_frames)
-    clip = clip.float()
-    clip = clip / 255.0
-
-    height, width = clip.shape[2], clip.shape[3]
-    # The format of boxes is [x1, y1, x2, y2]. The input boxes are in the
-    # range of [0, width] for x and [0,height] for y
-    boxes = clip_boxes_to_image(boxes, height, width)
-
-    # Resize short side to crop_size. Non-local and STRG uses 256.
-    clip, boxes = short_side_scale_with_boxes(
-        clip,
-        size=crop_size,
-        boxes=boxes,
-    )
-
-    # Normalize images by mean and std.
-    clip = normalize(
-        clip,
-        np.array(data_mean, dtype=np.float32),
-        np.array(data_std, dtype=np.float32),
-    )
-
-    boxes = clip_boxes_to_image(
-        boxes, clip.shape[2],  clip.shape[3]
-    )
-
-    # Incase of slowfast, generate both pathways
-    if slow_fast_alpha is not None:
-        fast_pathway = clip
-        # Perform temporal sampling from the fast pathway.
-        slow_pathway = torch.index_select(
-            clip,
-            1,
-            torch.linspace(
-                0, clip.shape[1] - 1, clip.shape[1] // slow_fast_alpha
-            ).long(),
-        )
-        clip = [slow_pathway, fast_pathway]
-
-    return clip, torch.from_numpy(boxes), ori_boxes
 
 
 out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'mp4v'), 30.0, (256, 256))
