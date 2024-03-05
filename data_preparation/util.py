e16c7f427ffc3d71f7576b2f476fd2bd5431a964
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-05 01:41:27 +0000

    more on testing

--- data_preparation/util.py
+++ data_preparation/util.py
@@ -1,4 +1,9 @@
 from torchvision.transforms import Compose, Resize, Normalize, ToTensor
+from pytorchvideo.transforms.functional import (
+    uniform_temporal_subsample,
+    short_side_scale_with_boxes,
+    clip_boxes_to_image,
+)
 import numpy as np
 from torch.utils.data import DataLoader,random_split
 import albumentations as A
@@ -88,6 +93,62 @@ def preprocess_transformer(roi):
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
     return transform(roi).unsqueeze(0)  # Add batch dimension
+
+def ava_inference_transform(
+    clip,
+    boxes,
+    num_frames = 4, #if using slowfast_r50_detection, change this to 32
+    crop_size = 256,
+    data_mean = [0.45, 0.45, 0.45],
+    data_std = [0.225, 0.225, 0.225],
+    slow_fast_alpha = None, #if using slowfast_r50_detection, change this to 4
+):
+
+    boxes = np.array(boxes)
+    ori_boxes = boxes.copy()
+
+    # Image [0, 255] -> [0, 1].
+    clip = uniform_temporal_subsample(clip, num_frames)
+    clip = clip.float()
+    clip = clip / 255.0
+
+    height, width = clip.shape[2], clip.shape[3]
+    # The format of boxes is [x1, y1, x2, y2]. The input boxes are in the
+    # range of [0, width] for x and [0,height] for y
+    boxes = clip_boxes_to_image(boxes, height, width)
+
+    # Resize short side to crop_size. Non-local and STRG uses 256.
+    clip, boxes = short_side_scale_with_boxes(
+        clip,
+        size=crop_size,
+        boxes=boxes,
+    )
+
+    # Normalize images by mean and std.
+    clip = normalize(
+        clip,
+        np.array(data_mean, dtype=np.float32),
+        np.array(data_std, dtype=np.float32),
+    )
+
+    boxes = clip_boxes_to_image(
+        boxes, clip.shape[2],  clip.shape[3]
+    )
+
+    # Incase of slowfast, generate both pathways
+    if slow_fast_alpha is not None:
+        fast_pathway = clip
+        # Perform temporal sampling from the fast pathway.
+        slow_pathway = torch.index_select(
+            clip,
+            1,
+            torch.linspace(
+                0, clip.shape[1] - 1, clip.shape[1] // slow_fast_alpha
+            ).long(),
+        )
+        clip = [slow_pathway, fast_pathway]
+
+    return clip, torch.from_numpy(boxes), ori_boxes
 
 
 def get_transformer(phase):
