74f45e7e0aca12ef8f025b67db76478c0a42f7ad
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-09 21:55:25 +0000

    train

--- model/slowfast_ava_model.py
+++ model/slowfast_ava_model.py
@@ -38,7 +38,7 @@ class SlowFastAva(LightningModule):
             sch.step(self.trainer.callback_metrics["valid_loss"])
 
     def training_step(self, batch, batch_idx):
-        print(f"Batch: {batch_idx}, {batch['video'].shape}, {batch['boxes'].shape}, {batch['labels'].shape}")
+        print(f"Batch: {batch_idx}, {batch['video'].shape}, {batch['boxes'].shape}, {batch['labels']}")
 
         outputs = self(batch["video"], batch["boxes"])
         loss = F.cross_entropy(outputs, batch["labels"])
