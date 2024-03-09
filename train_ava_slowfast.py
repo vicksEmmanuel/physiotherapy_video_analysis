62cacd7385b0ae813e509937d0e115541cfe2904
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-09 15:00:31 +0000

    minor change

--- train_ava_slowfast.py
+++ train_ava_slowfast.py
@@ -24,8 +24,6 @@ def train(config):
 
     print("Training begins:")
 
-    prepare_ava_dataset('train', config=config)
-
     loaders = {
         p: prepare_ava_dataset(p,  config=config)
             for p in [ 'train', 'val'] 
