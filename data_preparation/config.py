31b9ce44502a53a9ef9c3861970bcf20b8c7fccb
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-11 02:26:55 +0000

    train

--- data_preparation/config.py
+++ data_preparation/config.py
@@ -3,9 +3,9 @@ class CFG:
     num_frames = 4
     num_classes = 2
     batch_size = 4
-    num_workers = 32
+    num_workers = 0 #32
     learning_rate = 1e-4
-    num_epochs = 500
+    num_epochs = 100
     drop_prob = 0.
     dynamic_frames = True
     pretrained = True
