27dab2d700312d80dd51f3edee1bcdf59be806f1
Victor Umesiobi <Vicksemmanuel58@gmail.com>
2024-03-11 01:20:17 +0000

    train

--- model/slowfast_ava_model.py
+++ model/slowfast_ava_model.py
@@ -92,7 +92,7 @@ class SlowFastAva(LightningModule):
             bboxes = batch_item['boxes']
 
             labels = batch_item['labels']
-            labels = torch.tensor(labels, dtype=torch.long)
+            labels = torch.tensor(labels, dtype=torch.long, device=self.device)
             new_label = torch.nn.functional.one_hot(labels, self.num_classes + 1)
             labels = new_label
 
@@ -127,7 +127,7 @@ class SlowFastAva(LightningModule):
             bboxes = batch_item['boxes']
 
             labels = batch_item['labels']
-            labels = torch.tensor(labels, dtype=torch.long)
+            labels = torch.tensor(labels, dtype=torch.long, device=self.device)
             new_label = torch.nn.functional.one_hot(labels, self.num_classes + 1)
             labels = new_label
 
@@ -156,7 +156,7 @@ class SlowFastAva(LightningModule):
             bboxes = batch_item['boxes']
             
             labels = batch_item['labels']
-            labels = torch.tensor(labels, dtype=torch.long)
+            labels = torch.tensor(labels, dtype=torch.long, device=self.device)
             new_label = torch.nn.functional.one_hot(labels, self.num_classes + 1)
             labels = new_label
             
