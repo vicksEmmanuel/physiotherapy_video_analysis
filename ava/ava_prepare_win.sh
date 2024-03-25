00952d05e8daf9522fc15243159487fca6e87070
Victor Umesiobi <q2094871@tees.ac.uk>
2024-03-25 20:35:51 +0000

    test download

--- ava/ava_prepare_win.sh
+++ ava/ava_prepare_win.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+./download_win.sh
 ./cut_15min_30min_win.sh
 ./extract_frames.sh
 ./download_annotations.sh
---
+++ ava/download_win.sh
@@ -0,0 +1,14 @@
+DATA_DIR="videos"
+
+if [[ ! -d "${DATA_DIR}" ]]; then
+  echo "${DATA_DIR} doesn't exist. Creating it.";
+  mkdir -p ${DATA_DIR}
+fi
+
+# wget https://s3.amazonaws.com/ava-dataset/annotations/ava_file_names_trainval_v2.1.txt
+
+for line in $(cat ava_test.txt)
+do
+  $cleanedLine  = $(echo "$line" | sed 's/%0D//g')
+  wget https://s3.amazonaws.com/ava-dataset/trainval/$cleanedLine -P ${DATA_DIR}
+done
